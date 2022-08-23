bdb0f240288afc8987ef8a998f42ef66ff0245b0
johnsjohns <o-ana@hotmail.com>
2022-08-23 15:56:17 -0300

    Tentando o pyttsx3

--- voice.py
+++ voice.py
@@ -3,9 +3,9 @@ from gtts import gTTS
 from playsound import playsound
 
 audio = 'audio.mp3'
-language = 'pt-br'
+language = 'pt'
 
-sp = gTTS( text='Meu áudio em Python!', lang=language)
+sp = gTTS( text='Testando o texto para som', lang=language, tld='com.br')
 
 sp.save(audio)
 playsound(audio)
